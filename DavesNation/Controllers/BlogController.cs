60f1a0c2d904b304b8272780b8bf4ad5d599f922
David Stockdale <dlstockdale@gmail.com>
2015-03-12 21:01:19 -0700

    Start blog CRUD operations

---
+++ DavesNation/Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DavesNation.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult EditList()
+        {
+            return View();
+        }
+    }
+}
