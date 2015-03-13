5ffb0952b16942ff28a613bc08517e1423c6b36b
David Stockdale <dlstockdale@gmail.com>
2015-03-12 19:52:58 -0700

    Initial

---
+++ DavesNation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DavesNation.Startup))]
+namespace DavesNation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
